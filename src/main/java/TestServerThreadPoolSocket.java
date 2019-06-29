858d30daa4a57c42f637c35ce9ca70a8648168c1
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-06-29 09:50:06 +0200

    TestServerThreadPoolSocket extends Server

--- src/main/java/TestServerThreadPoolSocket.java
+++ src/main/java/TestServerThreadPoolSocket.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -9,46 +8,41 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 /**
  * Created by mtumilowicz on 2019-06-23.
  */
-public class TestServerThreadPoolSocket {
+public class TestServerThreadPoolSocket extends Server {
 
     private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(
-            100, 
-            100, 
+            100,
+            100,
             0L,
             MILLISECONDS,
             new ArrayBlockingQueue<>(1000),
-            (r, ex) -> { });
-
-    public static void main(String args[]) throws IOException {
-        final int portNumber = 81;
-        System.out.println("Creating server socket on port " + portNumber);
-        ServerSocket serverSocket = new ServerSocket(portNumber);
-        while (true) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Accepted connection from " + socket);
-            executor.execute(() -> {
-                try {
-                    OutputStream os = socket.getOutputStream();
-                    PrintWriter pw = new PrintWriter(os, true);
-                    pw.println("What's you name?");
-
-                    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    String str = br.readLine();
-
-                    pw.println("Hello, " + str);
-                    pw.close();
-                    socket.close();
-
-                    System.out.println("Just said hello to:" + str);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        socket.close();
-                    } catch (IOException ex) {
-                    }
-                }
+            (r, ex) -> {
             });
-        }
+
+    public static void main(String[] args) throws IOException {
+        new TestServerThreadPoolSocket().start();
     }
+
+    @Override
+    void handle(Socket client) {
+        executor.execute(() -> {
+            try (client) {
+                OutputStream os = client.getOutputStream();
+                PrintWriter pw = new PrintWriter(os, true);
+                pw.println("What's you name?");
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String str = br.readLine();
+
+                pw.println("Hello, " + str);
+                pw.close();
+                client.close();
+
+                System.out.println("Just said hello to:" + str);
+            } catch (IOException ex) {
+                // workshops
+            }
+        });
+    }
+
 }
