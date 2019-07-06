84bd0cd8eb582079a40d45a1c477c6310c4429db
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-06 23:28:58 +0200

    Server - using var

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -2,7 +2,6 @@ package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  * Created by mtumilowicz on 2019-06-29.
@@ -12,11 +11,11 @@ abstract class Server {
 
     void start() throws IOException {
         System.out.println("Creating server socket on port " + portNumber);
-        ServerSocket serverSocket = new ServerSocket(portNumber);
+        var serverSocket = new ServerSocket(portNumber);
         System.out.println("Created server socket on port " + portNumber);
 
         while (true) {
-            try(final Socket client = serverSocket.accept()) {
+            try(final var client = serverSocket.accept()) {
                 System.out.println("Accepted connection from " + client);
                 handle(new ClientConnection(client));
             }
