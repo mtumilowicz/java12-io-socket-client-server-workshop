eca57e211d3fbd36b8a9cda12972133f4fba2f7a
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-07 16:08:08 +0200

    Step9_ThreadPoolServerAnswer.java

---
+++ src/main/java/answers/server/Step9_ThreadPoolServerAnswer.java
@@ -0,0 +1,32 @@
+package answers.server;
+
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+/**
+ * Created by mtumilowicz on 2019-06-23.
+ */
+public class Step9_ThreadPoolServerAnswer extends Step6_ServerAnswer {
+
+    private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(
+            100,
+            100,
+            0L,
+            MILLISECONDS,
+            new ArrayBlockingQueue<>(1000),
+            new ThreadPoolExecutor.DiscardPolicy()
+    );
+
+    public static void main(String[] args) throws IOException {
+        new Step9_ThreadPoolServerAnswer().start();
+    }
+
+    @Override
+    void handle(Runnable clientConnection) {
+        executor.execute(clientConnection);
+    }
+
+}
