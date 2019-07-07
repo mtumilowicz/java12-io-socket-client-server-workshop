3ade11fc6f491bc124f8d327fbb17110b4bfc289
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-07 15:55:28 +0200

    Step3_ClientConnection.java, Step4_SingleThreadedServerAnswer.java, Step5_ThreadPerRequestServerAnswer.java

---
+++ src/main/java/answers/server/Step3_ClientConnection.java
@@ -0,0 +1,48 @@
+package answers.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by mtumilowicz on 2019-07-07.
+ */
+public class Step3_ClientConnection implements Runnable {
+    private final Socket client;
+    private final PrintWriter writer;
+    private final BufferedReader reader;
+
+    Step3_ClientConnection(Socket client) throws IOException {
+        this.client = client;
+        this.writer = new PrintWriter(client.getOutputStream(), true);
+        this.reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    }
+
+    @Override
+    public void run() {
+        try (client; writer; reader) {
+            sendLine("What's you name?");
+
+            var name = readLine();
+            sendLine("Hello, " + name);
+
+            log("Just said hello to:" + name);
+        } catch (IOException exception) {
+            // workshops
+        }
+    }
+
+    private void sendLine(String message) {
+        writer.println(message);
+    }
+
+    private String readLine() throws IOException {
+        return reader.readLine();
+    }
+
+    private void log(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/answers/server/Step4_SingleThreadedServerAnswer.java
@@ -0,0 +1,36 @@
+package answers.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Created by mtumilowicz on 2019-06-23.
+ */
+public class Step4_SingleThreadedServerAnswer {
+
+    final int portNumber = 81;
+
+    public static void main(String[] args) throws IOException {
+        new Step4_SingleThreadedServerAnswer().start();
+    }
+
+    void start() throws IOException {
+        log("Creating server socket on port " + portNumber);
+        var serverSocket = new ServerSocket(portNumber);
+        log("Created server socket on port " + portNumber);
+
+        while (true) {
+            try (final var client = serverSocket.accept()) {
+                log("Accepted connection from " + client);
+
+                new Step3_ClientConnection(client);
+            } catch (IOException exception) {
+                // workshops
+            }
+        }
+    }
+
+    private void log(String message) {
+        System.out.println(message);
+    }
+}
