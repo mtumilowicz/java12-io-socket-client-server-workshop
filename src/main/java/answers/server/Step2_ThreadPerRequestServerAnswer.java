46b1d8d6d426ccb487198d52ef84d970f762ca07
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-07 15:50:35 +0200

    Step2_ThreadPerRequestServerAnswer.java

---
+++ src/main/java/answers/server/Step2_ThreadPerRequestServerAnswer.java
@@ -0,0 +1,51 @@
+package answers.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+
+/**
+ * Created by mtumilowicz on 2019-06-23.
+ */
+public class Step2_ThreadPerRequestServerAnswer {
+
+    final int portNumber = 81;
+
+    public static void main(String[] args) throws IOException {
+        new Step2_ThreadPerRequestServerAnswer().start();
+    }
+
+    void start() throws IOException {
+        log("Creating server socket on port " + portNumber);
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        log("Created server socket on port " + portNumber);
+
+        while (true) {
+            try (final var client = serverSocket.accept()) {
+                log("Accepted connection from " + client);
+                new Thread(() -> {
+                    try (final var writer = new PrintWriter(client.getOutputStream(), true);
+                         final var reader = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+                        writer.println("What's you name?");
+
+                        var name = reader.readLine();
+                        writer.println("Hello, " + name);
+
+                        log("Just said hello to:" + name);
+                    } catch (IOException exception) {
+                        // workshops
+                    }
+                });
+            } catch (IOException exception) {
+                // workshops
+            }
+        }
+
+    }
+
+    private void log(String message) {
+        System.out.println(message);
+    }
+}
