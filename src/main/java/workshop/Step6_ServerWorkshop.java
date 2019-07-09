9416edef26370b670c9f3e297d45bd85ead7084b
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-09 21:48:50 +0200

    Step6_ServerWorkshop.java update

--- src/main/java/workshop/Step6_ServerWorkshop.java
+++ src/main/java/workshop/Step6_ServerWorkshop.java
@@ -6,7 +6,16 @@ import java.io.IOException;
  * Created by mtumilowicz on 2019-06-29.
  */
 abstract class Step6_ServerWorkshop {
-    private final int portNumber = 81;
+    
+    private final int portNumber;
+
+    Step6_ServerWorkshop(int portNumber) {
+        this.portNumber = portNumber;
+    }
+
+    Step6_ServerWorkshop() {
+        this.portNumber = 81;
+    }
 
     /**
      * move all common code of start() methods in {@link Step4_SingleThreadedServerWorkshop}, 
